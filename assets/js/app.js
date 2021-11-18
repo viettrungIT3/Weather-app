5547b6e8a91dd38a9e2209828897fd13000662c0
viettrungIT3 <viettrungcntt03@gmail.com>
2021-11-18 10:31:03 +0700

    Weather app - SpeechRecognition

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,5 @@
+let long;
+let lat;
 
 const APP_ID = '73af23c1d247a435aa8dce19fc83c958';
 const DEFAULT_VALUE = '--';
@@ -13,11 +15,27 @@ const sunset = document.querySelector('.sunset');
 const humidity = document.querySelector('.humidity');
 const windSpeed = document.querySelector('.wind-speed');
 
+const microphone = document.querySelector('.microphone');
+
+window.addEventListener('change', () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(position => {
+            // console.log(position);
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            console.log(long);
+            console.log(lat);
+
+        });
+    }
+});
+
 
 searchInput.addEventListener('change', (e) => {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&appid=${APP_ID}&units=metric&lang=vi`)
         .then(async res => {
             const data = await res.json();
+            console.log(data);
             console.log('[Search Input]', data);
             cityName.innerHTML = data.name || DEFAULT_VALUE;
             weatherState.innerHTML = data.weather[0].description || DEFAULT_VALUE;
@@ -30,3 +48,58 @@ searchInput.addEventListener('change', (e) => {
             windSpeed.innerHTML = (data.wind.speed * 3.6).toFixed(2) || DEFAULT_VALUE;
         });
 });
+
+// Trợ lý ảo bằng giọng nói
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+
+recognition.lang = 'vi-VI';         // Nhận diện tiếng việt
+recognition.continuous = false;     // Kết quả trả về luôn - results are returned for each recognition, or only a single result.
+
+microphone.addEventListener('click', (e) => {
+    e.preventDefault;
+
+    recognition.start();
+    microphone.classList.add('recording');
+})
+
+recognition.onspeechend = () => {
+    recognition.stop();
+    microphone.classList.remove('recording');
+}
+
+recognition.onerror = (err) => {
+    console.log(err);
+    microphone.classList.remove('recording');
+}
+
+recognition.onresult = (e) => {
+    console.log('onresult', e);
+    const text = e.results[0][0].transcript;
+    handleVoice(text);
+}
+
+const handleVoice = (text) => {
+    console.log('text:', text);
+
+    const handleText = text.toLowerCase();
+    if ( handleText.includes('thời tiết tại')) {
+        const location = handleText.split('tại')[1].trim();
+
+        console.log('location:', location);
+        searchInput.value = location;
+        const changeEvent = new Event('change');
+        searchInput.dispatchEvent(changeEvent);
+    }
+}
+
+
+
+
+
+
+
+
+
+
